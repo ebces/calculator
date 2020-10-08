b4cfda446f9e04b824d4940ea25085cf0857dc59
Ebces <ptashinsky@tut.by>
2020-10-08 15:38:06 +0300

    feat: added keyboard control

--- script.js
+++ script.js
@@ -7,54 +7,77 @@ let displayedValue = 0;
 let isOperationClicked = false;
 let operation = '';
 
+const dataInput = (value) => {
+  if (isOperationClicked) {
+    display.textContent = '';
+    isOperationClicked = false;
+  }
+  if (display.textContent === '0') {
+    display.textContent = '';
+  }
+  if (display.textContent.length < 10 && value !== '.') {
+    display.textContent += value;
+  }
+  if (display.textContent.indexOf('.') === -1 && value === '.') {
+    if (display.textContent === '') {
+      display.textContent += '0.';
+    } else {
+      display.textContent += '.';
+    }
+  }
+}
+
+const changeOperation = (value) => {
+  if (!operation) {
+    displayedValue = Number(display.textContent);
+  }
+  if (!isOperationClicked) {
+    switch (operation) {
+      case '+':
+        displayedValue += Number(display.textContent);
+        break;
+      case '-':
+        displayedValue -= Number(display.textContent);
+        break;
+      case '/':
+        displayedValue /= Number(display.textContent);
+        break;
+      case '*':
+        displayedValue *= Number(display.textContent);
+        break;
+      case '=':
+        displayedValue = Number(display.textContent);
+        break;
+      case 'Enter':
+        displayedValue = Number(display.textContent);
+        break;
+    }
+  }
+  isOperationClicked = true;
+  operation = value;
+  display.textContent = String(displayedValue).length > 10 ? String(displayedValue).slice(0, 10) : displayedValue;
+}
+
+
+document.addEventListener('keyup', (e) => {
+  if (/([0-9]|\.)/.test(e.key)) {
+    dataInput(e.key);
+  }
+  if (/(\/|\*|\-|\+|Enter)/.test(e.key)) {
+    changeOperation(e.key);
+  }
+});
+
 numbers.forEach((number) => {
   number.addEventListener('click', (e) => {
-    if (isOperationClicked) {
-      display.textContent = '';
-      isOperationClicked = false;
-    }
-    if (display.textContent === '0') {
-      display.textContent = '';
-    }
-    if (display.textContent.length < 10 && e.target.textContent !== '.') {
-      display.textContent += e.target.innerText;
-    }
-    if (display.textContent.indexOf('.') === -1 && e.target.textContent === '.') {
-      if (display.textContent === '') {
-        display.textContent += '0.';
-      } else {
-        display.textContent += '.';
-      }
-    }
+    dataInput(e.target.textContent);
   });
 });
 
+
 operations.forEach((oper) => {
   oper.addEventListener('click', (e) => {
-    if (!operation) {
-      displayedValue = Number(display.textContent);
-    }
-    if (!isOperationClicked) {
-      switch (operation) {
-        case '+':
-          displayedValue += Number(display.textContent);
-          break;
-        case '-':
-          displayedValue -= Number(display.textContent);
-          break;
-        case '/':
-          displayedValue /= Number(display.textContent);
-          break;
-        case '*':
-          displayedValue *= Number(display.textContent);
-          break;
-        case '=':
-          displayedValue = Number(display.textContent);
-      }
-    }
-    isOperationClicked = true;
-    operation = e.target.textContent;
-    display.textContent = String(displayedValue).length > 10 ? String(displayedValue).slice(0,10) : displayedValue;
+    changeOperation(e.target.textContent);
   });
 });
 
